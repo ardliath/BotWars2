ac8bb724a09dbb5740fbf2d799ddde4c23b017cf
Adam Griffiths <Adam.Griffiths@callcreditgroup.com>
2018-12-03 15:54:45 +0000

    Added GameManagerClass

---
+++ Code/BotWars2Server/Code/Logic/GameManager.cs
@@ -0,0 +1,17 @@
+﻿using BotWars2Server.Code.State;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotWars2Server.Code.Logic
+{
+    public class GameManager
+    {
+        public GameManager(Arena arena, params Player[] players)
+        {
+
+        }
+    }
+}
--- Code/BotWars2Server/Code/State/Arena.cs
+++ Code/BotWars2Server/Code/State/Arena.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BotWars2Server.Tests
+namespace BotWars2Server.Code.State
 {
-    public class Class1
+    public class Arena
     {
     }
 }
