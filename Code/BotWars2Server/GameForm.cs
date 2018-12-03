318d708c007814e787abffcbd3c35f8244074543
Adam Griffiths <Adam.Griffiths@callcreditgroup.com>
2018-12-03 15:52:36 +0000

    Added basic forms

---
+++ Code/BotWars2Server/GameForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BotWars2Server
+{
+    public partial class GameForm : Form
+    {
+        public GameForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
